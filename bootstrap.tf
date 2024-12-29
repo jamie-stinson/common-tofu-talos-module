b7414ca3fcf2693680426075dc03d654b02e80c7
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2024-12-29 15:43:25 +0000

    feat: refactor (#7)

--- bootstrap.tf
+++ bootstrap.tf
@@ -9,7 +9,7 @@ data "talos_client_configuration" "this" {
 }
 
 resource "talos_machine_bootstrap" "this" {
-  depends_on = [talos_machine_configuration_apply.this]
+  depends_on           = [talos_machine_configuration_apply.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = [for k, v in var.talos.node_data.control_plane.nodes : k][0]
 }
@@ -17,5 +17,16 @@ resource "talos_machine_bootstrap" "this" {
 resource "talos_cluster_kubeconfig" "this" {
   depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
-  node                 = var.talos.cluster.vip_address
+  node                 = var.talos.cluster.api_server
+}
+
+data "talos_cluster_health" "this" {
+  depends_on           = [talos_machine_bootstrap.this]
+  worker_nodes         = [for k, v in var.talos.node_data.worker.nodes : k]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  control_plane_nodes  = [for k, v in var.talos.node_data.control_plane.nodes : k]
+  endpoints            = [for k, v in var.talos.node_data.control_plane.nodes : k]
+  timeouts             = {
+    read = "2h"
+  }
 }
