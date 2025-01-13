fe364d87ec8065a4a7cc5ae8d862dc1cb6681120
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2025-01-13 21:05:10 +0000

    feat: code refactor (#14)

--- bootstrap.tf
+++ bootstrap.tf
@@ -5,28 +5,17 @@ resource "talos_machine_secrets" "this" {
 data "talos_client_configuration" "this" {
   cluster_name         = var.talos.cluster.name
   client_configuration = talos_machine_secrets.this.client_configuration
-  endpoints            = [for k, v in var.talos.node_data.control_plane.nodes : k]
+  endpoints            = var.talos.cluster.compute.control_plane.nodes
 }
 
 resource "talos_machine_bootstrap" "this" {
   depends_on           = [talos_machine_configuration_apply.this]
   client_configuration = talos_machine_secrets.this.client_configuration
-  node                 = [for k, v in var.talos.node_data.control_plane.nodes : k][0]
+  node                 = var.talos.cluster.compute.control_plane.nodes[0]
 }
 
 resource "talos_cluster_kubeconfig" "this" {
   depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
-  node                 = var.talos.cluster.api_server
-}
-
-data "talos_cluster_health" "this" {
-  depends_on           = [talos_machine_bootstrap.this]
-  worker_nodes         = [for k, v in var.talos.node_data.worker.nodes : k]
-  client_configuration = talos_machine_secrets.this.client_configuration
-  control_plane_nodes  = [for k, v in var.talos.node_data.control_plane.nodes : k]
-  endpoints            = [for k, v in var.talos.node_data.control_plane.nodes : k]
-  timeouts             = {
-    read = "2h"
-  }
+  node                 = var.talos.cluster.api_server_endpoint
 }
